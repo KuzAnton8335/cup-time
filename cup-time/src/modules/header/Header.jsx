e3da56475cfbe09cd265a0e840fe4ba1b850d33a
KuzAnton8335 <ant2783@gmail.com>
2024-08-16 15:48:22 +0300

    dev mobail menu

--- cup-time/src/modules/header/Header.jsx
+++ cup-time/src/modules/header/Header.jsx
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useCart } from "../../context/CartContext";
 import { useProducts } from "../../context/ProductContext";
 import "./header.scss";
-
+// !!остановился на стилях работа по замене стилей
 export const Header = () => {
 	const { cart = [] } = useCart();
 	const location = useLocation();
@@ -11,8 +12,16 @@ export const Header = () => {
 			URLSearchParams(location.search).get("category");
 		return currentCategory === category ? "active" : '';
 	}
-
 	const { categories } = useProducts();
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+	const openMenu = () => {
+		setIsMenuOpen(false)
+	}
+
+	const closeMenu = () => {
+		setIsMenuOpen(true)
+	}
 	return (
 		<>
 			<header className="header">
@@ -21,13 +30,14 @@ export const Header = () => {
 						<img src="./img/logo.svg" alt="Логотип cup time" className="header__logo-img" />
 					</Link>
 
-					<nav className="header__nav">
+					<nav className={`header__nav ${isMenuOpen ? "active" : ''}`}>
 						<ul className="header__menu">
 							{Object.entries(categories).map(([key, value]) => (
 								<li key={key} className="header__menu-item">
 									<Link to={`/products?category=${key}`}
 										className={`header__menu-link
-									${getActiveClass(key)}`}>{value}</Link>
+									${getActiveClass(key)}`} onClick={closeMenu}>
+										{value}</Link>
 								</li>
 							))}
 							{/* <li className="header__menu-item">
@@ -56,13 +66,23 @@ export const Header = () => {
 										${getActiveClass('other')}`}>Прочее</Link>
 							</li> */}
 						</ul>
+						<button className="header__close-btn" onClick={closeMenu}>
+							<svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<rect x="7.28174" y="7.07532" width="20" height="1" transform="rotate(45 7.28174 7.07532)" fill="#D9D9D9" />
+								<rect x="6.5752" y="21.2173" width="20" height="1" transform="rotate(-45 6.5752 21.2173)" fill="#D9D9D9" />
+							</svg>
+						</button>
 					</nav>
-					<div className="header__mobail-menu">
+					<div className="header__control">
 						<Link to="cart" className="header__cart-link">
 							{cart ? cart.reduce((acc, item) => item.quantity + acc, 0) : 0}
 						</Link>
-						<button className="header__burger">
-							<span className="header__burger-line"></span>
+						<button className="header__burger" aria-label="Открыть меню" onClick={openMenu}>
+							<svg className="header__burger-icon" width="28" height="29" viewBox="0 0 28 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<rect x="4" y="9.5" width="20" height="1" fill="#D9D9D9" />
+								<rect x="4" y="14.5" width="20" height="1" fill="#D9D9D9" />
+								<rect x="4" y="19.5" width="20" height="1" fill="#D9D9D9" />
+							</svg>
 						</button>
 					</div>
 				</div>
